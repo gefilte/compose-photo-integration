6c7cdd3ee8f527247564a60266c76ec71ad7e26f
David Pisoni <david_pisoni@intuit.com>
2021-08-19 15:07:33 -0700

    Initial Commit: Permissions

---
+++ app/src/main/java/com/example/composephoto/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.example.composephoto
+
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.provider.Settings
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import com.example.composephoto.ui.theme.ComposePhotoIntegrationTheme
+import com.example.composephoto.util.Permission
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+
+@ExperimentalPermissionsApi
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposePhotoIntegrationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    MainContent(Modifier.fillMaxSize())
+                }
+            }
+        }
+    }
+}
+
+@ExperimentalPermissionsApi
+@Composable
+fun MainContent(modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+    Permission(
+        permission = Manifest.permission.CAMERA,
+        rationale = "You said you wanted a picture, so I'm going to have to ask for permission.",
+        permissionNotAvailableContent = {
+            Column(modifier) {
+                Text("O noes! No Camera!")
+                Spacer(modifier = Modifier.height(8.dp))
+                Button(
+                    onClick = {
+                        context.startActivity(
+                            Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+                                data = Uri.fromParts("package", context.packageName, null)
+                            }
+                        )
+                    }
+                ) {
+                    Text("Open Settings")
+                }
+            }
+        }
+    ) {
+        Text("It worked!")
+    }
+}
