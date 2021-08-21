8ff57b1e6fd67c51d3b42826fceaf155d282ca7f
David Pisoni <david_pisoni@intuit.com>
2021-08-20 18:26:06 -0700

    Added GallerySelect

--- app/src/main/java/com/example/composephoto/MainActivity.kt
+++ app/src/main/java/com/example/composephoto/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -18,10 +19,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.core.net.toUri
 import coil.annotation.ExperimentalCoilApi
 import coil.compose.rememberImagePainter
 import com.example.composephoto.camera.CameraCapture
+import com.example.composephoto.gallery.GallerySelect
 import com.example.composephoto.ui.theme.ComposePhotoIntegrationTheme
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -48,9 +51,8 @@ class MainActivity : ComponentActivity() {
 @ExperimentalPermissionsApi
 @Composable
 fun MainContent(modifier: Modifier = Modifier) {
-    val emptyImageUri = Uri.parse("file://dev/null")
-    var imageUri by remember { mutableStateOf(emptyImageUri) }
-    if (imageUri != emptyImageUri) {
+    var imageUri by remember { mutableStateOf(EMPTY_IMAGE_URI) }
+    if (imageUri != EMPTY_IMAGE_URI) {
         Box(modifier = modifier) {
             Image(
                 modifier = Modifier.fillMaxSize(),
@@ -60,18 +62,43 @@ fun MainContent(modifier: Modifier = Modifier) {
             Button(
                 modifier = Modifier.align(Alignment.BottomCenter),
                 onClick = {
-                    imageUri = emptyImageUri
+                    imageUri = EMPTY_IMAGE_URI
                 }
             ) {
                 Text("Remove image")
             }
         }
     } else {
-        CameraCapture(
-            modifier = modifier,
-            onImageFile = { file ->
-                imageUri = file.toUri()
+        var showGallerySelect by remember { mutableStateOf(false) }
+        if (showGallerySelect) {
+            GallerySelect(
+                modifier = modifier,
+                onImageUri = { uri ->
+                    showGallerySelect = false
+                    imageUri = uri
+                }
+            )
+        } else {
+            Box(modifier = modifier) {
+                CameraCapture(
+                    modifier = modifier,
+                    onImageFile = { file ->
+                        imageUri = file.toUri()
+                    }
+                )
+                Button(
+                    modifier = Modifier
+                        .align(Alignment.TopCenter)
+                        .padding(4.dp),
+                    onClick = {
+                        showGallerySelect = true
+                    }
+                ) {
+                    Text("Select from Gallery")
+                }
             }
-        )
+        }
     }
 }
+
+val EMPTY_IMAGE_URI: Uri = Uri.parse("file://dev/null")
---
+++ app/src/main/java/com/example/composephoto/gallery/GallerySelect.kt
@@ -0,0 +1,86 @@
+package com.example.composephoto.gallery
+
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.provider.Settings
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import com.example.composephoto.EMPTY_IMAGE_URI
+import com.example.composephoto.util.Permission
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+
+@ExperimentalPermissionsApi
+@Composable
+fun GallerySelect(
+    modifier: Modifier = Modifier,
+    onImageUri: (Uri) -> Unit = { }
+) {
+    val context = LocalContext.current
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent(),
+        onResult = { uri: Uri? ->
+            onImageUri(uri ?: EMPTY_IMAGE_URI)
+        }
+    )
+
+    @Composable
+    fun LaunchGallery() {
+        SideEffect {
+            launcher.launch("image/*")
+        }
+    }
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+        Permission(
+            permission = Manifest.permission.ACCESS_MEDIA_LOCATION,
+            rationale = "You want to read from photo gallery, so I'm going to have to ask for permission.",
+            permissionNotAvailableContent = {
+                Column(modifier) {
+                    Text("O noes! No Photo Gallery!")
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Row {
+                        Button(
+                            modifier = Modifier.padding(4.dp),
+                            onClick = {
+                                context.startActivity(
+                                    Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+                                        data = Uri.fromParts("package", context.packageName, null)
+                                    }
+                                )
+                            }
+                        ) {
+                            Text("Open Settings")
+                        }
+                        // If they don't want to grant permissions, this button will result in going back
+                        Button(
+                            modifier = Modifier.padding(4.dp),
+                            onClick = {
+                                onImageUri(EMPTY_IMAGE_URI)
+                            }
+                        ) {
+                            Text("Use Camera")
+                        }
+                    }
+                }
+            },
+        ) {
+            LaunchGallery()
+        }
+    } else {
+        LaunchGallery()
+    }
+}
